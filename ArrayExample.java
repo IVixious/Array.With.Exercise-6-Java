04ca94998db307a6ff62a1b6bb3c93fc03bb71fd
IVixious <valerisha.kmr@gmail.com>
2025-08-28 08:48:05 +0800

    Add files via upload

---
+++ ArrayExample.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] num = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            for (int i = 0; i < 10; i++) {
+                System.out.print("Enter number ");
+                num[i] = sc.nextInt();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Invalid index entered");
+        }
+
+        // Finding the largest number
+        int largest = num[0];
+        for (int i = 1; i < num.length; i++) {
+            if (num[i] > largest) {
+                largest = num[i];
+            }
+        }
+
+        System.out.println("The largest number is: " + largest);
+
+    }
+}
